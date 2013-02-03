aa84385e074a6d9005205bf8b330c8ec15e1cb7b
jialiya <jialiya.huang@students.olin.edu>
2013-02-03 01:20:45 -0500

    sorting tweets

--- routes/tweets.js
+++ routes/tweets.js
@@ -7,12 +7,12 @@ exports.new = function(req, res){
     return console.log("no one is logged in");
 
   // create the tweet
-  var tweet = new Tweet({tweet: req.body.tweet, _creator: user._id});
+  var tweet = new Tweet({tweet: req.body.tweet, created: new Date(), _creator: user._id});
   tweet.save(function (err) {
     if (err)
       return console.log("couldn't save tweet", err);
 
-    res.send("yay");
+    res.render('_tweets', {tweets:[tweet]});
   });
 };
 
@@ -29,13 +29,13 @@ exports.list = function(req, res){
   getTweets(function (tweets) {
     console.log("tweets", tweets);
     res.render('tweets', {tweets:tweets, 
-        logged_in: req.session.user ? true : false});
+        logged_in: req.session.user});
   });
 };
 
 // helpers
 function getTweets (next) {
-  Tweet.find().sort("created").limit(20).exec(
+  Tweet.find().sort("-created").populate("_creator").limit(20).exec(
     function (err, tweets) {
       if (err)
         return console.log("can't find tweets", err);
